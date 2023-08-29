04d428d8d748abda77874fab2feeb2bbb0504189
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-29 14:14:04 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/catalisa/sotckz/SotckzApplication.java
@@ -0,0 +1,13 @@
+package br.com.catalisa.sotckz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotckzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotckzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/catalisa/sotckz/SotckzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalisa.sotckz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotckzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
