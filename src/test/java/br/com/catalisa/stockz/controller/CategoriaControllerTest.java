d42b56481c767e3a63c340e0e3ae69549c5b43d8
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-31 09:06:14 -0300

    :test_tube: test: criado novos testes

---
+++ src/test/java/br/com/catalisa/stockz/controller/CategoriaControllerTest.java
@@ -0,0 +1,187 @@
+package br.com.catalisa.stockz.controller;
+
+import br.com.catalisa.stockz.exception.CategoriaJaCadastradaException;
+import br.com.catalisa.stockz.exception.EntidadeNaoEncontradaException;
+import br.com.catalisa.stockz.model.dto.CategoriaDTO;
+import br.com.catalisa.stockz.service.CategoriaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoriaController.class)
+public class CategoriaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CategoriaService categoriaService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    private CategoriaDTO categoriaDTO1;
+    private CategoriaDTO categoriaDTO1Repetida;
+    private CategoriaDTO categoriaDTO2;
+
+    @BeforeEach
+    public void config(){
+
+        categoriaDTO1 = new CategoriaDTO("ferramentas");
+        categoriaDTO1Repetida = new CategoriaDTO("ferramentas");
+        categoriaDTO2 = new CategoriaDTO("brinquedos");
+
+    }
+
+    @Test
+    @DisplayName("Buscar todos as categorias ")
+    public void buscarTodasAsCategoriasTeste() throws Exception {
+
+        List<CategoriaDTO> categoriasEsperadas = new ArrayList<>();
+        categoriasEsperadas.add(categoriaDTO1);
+        categoriasEsperadas.add(categoriaDTO2);
+
+        when(categoriaService.listarTodos()).thenReturn(categoriasEsperadas);
+
+        String responseJson = objectMapper.writeValueAsString(categoriasEsperadas);
+
+        mockMvc.perform(get("/api/categorias")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Buscar categoria por ID existente")
+    public void buscarCategoriaPorIDExistenteTeste() throws Exception {
+
+        Long idCategoria = 1L;
+
+        when(categoriaService.listarPorId(idCategoria)).thenReturn(categoriaDTO1);
+
+        String responseJson = objectMapper.writeValueAsString(categoriaDTO1);
+
+        mockMvc.perform(get("/api/categorias/{id}", idCategoria)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Buscar categoria por ID inexistente")
+    public void buscarCategoriaPorIDInexistenteTeste() throws Exception {
+
+        Long idCategoria = 1L;
+
+        when(categoriaService.listarPorId(idCategoria)).thenThrow(new EntidadeNaoEncontradaException("Categoria não encontrada"));
+
+        mockMvc.perform(get("/api/categorias/{id}", idCategoria)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Categoria não encontrada"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Cadastrar nova Categoria")
+    public void cadastrarCategoriaTeste() throws Exception {
+
+        when(categoriaService.criar(Mockito.any(categoriaDTO1.getClass()))).thenReturn(categoriaDTO1);
+
+        String requestJson = objectMapper.writeValueAsString(categoriaDTO1);
+        String responseJson = objectMapper.writeValueAsString(categoriaDTO1);
+
+        mockMvc.perform(post("/api/categorias")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+
+        verify(categoriaService, times(1)).criar(Mockito.any());
+
+    }
+
+    @Test
+    @DisplayName("Cadastrar nova Categoria Duplicada")
+    public void cadastrarCategoriaDuplicadaTeste() throws Exception {
+
+        when(categoriaService.criar(Mockito.any(categoriaDTO1.getClass()))).thenThrow(new CategoriaJaCadastradaException("Categoria com esse nome já está cadastrada"));
+
+        String requestJson = objectMapper.writeValueAsString(categoriaDTO1);
+
+        mockMvc.perform(post("/api/categorias")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+
+        verify(categoriaService, times(1)).criar(Mockito.any());
+
+    }
+
+    @Test
+    @DisplayName("Atualizar um produto")
+    public void alterarCategoriaTest() throws Exception {
+
+        Long idCategoria = 1L;
+        when(categoriaService.atualizar(Mockito.any(), Mockito.any(categoriaDTO2.getClass()))).thenReturn(categoriaDTO2);
+
+        String requestJson = objectMapper.writeValueAsString(categoriaDTO2);
+        String responseJson = objectMapper.writeValueAsString(categoriaDTO2);
+
+        mockMvc.perform(put("/api/categorias/{id}", idCategoria)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+
+        verify(categoriaService, times(1)).atualizar(Mockito.any(), Mockito.any(categoriaDTO2.getClass()));
+    }
+
+    @Test
+    @DisplayName("Deletar uma categoria existente")
+    public void deletarUmaCategoriaTest() throws Exception{
+
+        mockMvc.perform(delete("/api/categorias/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        Mockito.verify(categoriaService, times(1)).deletar(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deletar uma Categoria inexistente")
+    public void deletarUmaCategoriaInexistenteTest() throws Exception{
+
+        doThrow(new EntidadeNaoEncontradaException("Categoria não encontrada"))
+                .when(categoriaService).deletar(1L);
+
+        mockMvc.perform(delete("/api/caegorias/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+}
--- src/test/java/br/com/catalisa/stockz/controller/ProdutoControllerTest.java
+++ src/test/java/br/com/catalisa/stockz/controller/ProdutoControllerTest.java
@@ -2,14 +2,14 @@ package br.com.catalisa.stockz.controller;
 
 import br.com.catalisa.stockz.exception.EntidadeNaoEncontradaException;
 import br.com.catalisa.stockz.model.Categoria;
-import br.com.catalisa.stockz.model.dto.ProdutosDTO;
-import br.com.catalisa.stockz.repository.ProdutosRepository;
-import br.com.catalisa.stockz.service.ProdutosService;
+import br.com.catalisa.stockz.model.dto.ProdutoDTO;
+import br.com.catalisa.stockz.service.ProdutoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,31 +20,29 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ProdutosController.class)
+@WebMvcTest(ProdutoController.class)
 public class ProdutoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ProdutosController produtosController;
+    private ProdutoController produtosController;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @MockBean
-    private ProdutosService produtosService;
+    private ProdutoService produtoService;
 
-    @MockBean
-    private ProdutosRepository produtosRepository;
 
-    private ProdutosDTO produtoDTO1;
-    private ProdutosDTO produtoDTO2;
+    private ProdutoDTO produtoDTO1;
+    private ProdutoDTO produtoDTO2;
     private Categoria categoria1;
 
     @BeforeEach
@@ -52,59 +50,156 @@ public class ProdutoControllerTest {
         categoria1 = new Categoria();
         categoria1.setNome("ferramentas");
 
-        produtoDTO1 = new ProdutosDTO("computador", "30gb de ram", categoria1, BigDecimal.valueOf(3000));
-        produtoDTO2 = new ProdutosDTO("notebook", "15gb de ram", categoria1, BigDecimal.valueOf(5000));
+        produtoDTO1 = new ProdutoDTO("computador", "30gb de ram", categoria1, BigDecimal.valueOf(3000));
+        produtoDTO2 = new ProdutoDTO("notebook", "15gb de ram", categoria1, BigDecimal.valueOf(5000));
 
     }
     @Test
     @DisplayName("Buscar todos os produtos ")
-    public void buscarTodasAsContasTeste() throws Exception {
+    public void buscarTodasOsProdutosTeste() throws Exception {
 
-        List<ProdutosDTO> produtosEsperados = new ArrayList<>();
+        List<ProdutoDTO> produtosEsperados = new ArrayList<>();
         produtosEsperados.add(produtoDTO1);
         produtosEsperados.add(produtoDTO2);
 
-        when(produtosService.listarTodos()).thenReturn(produtosEsperados);
+        when(produtoService.listarTodos()).thenReturn(produtosEsperados);
 
-        String expectedJson = objectMapper.writeValueAsString(produtosEsperados);
+        String responseJson = objectMapper.writeValueAsString(produtosEsperados);
 
         mockMvc.perform(get("/api/produtos")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
+                .andExpect(content().json(responseJson))
                 .andDo(print());
     }
 
     @Test
     @DisplayName("Buscar produto por ID existente")
-    public void buscarContaPorIDExistenteTeste() throws Exception {
+    public void buscarProdutoPorIDExistenteTeste() throws Exception {
 
         Long idProduto = 1L;
 
-        when(produtosService.listarPorId(idProduto)).thenReturn(produtoDTO1);
+        when(produtoService.listarPorId(idProduto)).thenReturn(produtoDTO1);
 
-        String expectedJson = objectMapper.writeValueAsString(produtoDTO1);
+        String responseJson = objectMapper.writeValueAsString(produtoDTO1);
 
         mockMvc.perform(get("/api/produtos/{id}", idProduto)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
+                .andExpect(content().json(responseJson))
                 .andDo(print());
     }
 
     @Test
     @DisplayName("Buscar produto por ID inexistente")
-    public void buscarContaPorIDInexistenteTeste() throws Exception {
+    public void buscarProdutoPorIDInexistenteTeste() throws Exception {
 
         Long idProduto = 1L;
 
-        when(produtosService.listarPorId(idProduto)).thenThrow(new EntidadeNaoEncontradaException("Produto não encontrado"));
+        when(produtoService.listarPorId(idProduto)).thenThrow(new EntidadeNaoEncontradaException("Produto não encontrado"));
 
         mockMvc.perform(get("/api/produtos/{id}", idProduto)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Produto não encontrado"))
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Buscar produto por Nome existente")
+    public void buscarProdutoPorNomeExistenteTeste() throws Exception {
+
+        String nomeProduto = "computador";
+
+        when(produtoService.listarPorNome(nomeProduto)).thenReturn(produtoDTO1);
+
+        String responseJson = objectMapper.writeValueAsString(produtoDTO1);
+
+        mockMvc.perform(get("/api/produtos/nome/{nome}", nomeProduto)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Buscar produto por Nome inexistente")
+    public void buscarProdutoPorNomeInexistenteTeste() throws Exception {
+
+        String nomeProduto = "celular";
+
+        when(produtoService.listarPorNome(nomeProduto)).thenThrow(new EntidadeNaoEncontradaException("Produto não encontrado"));
+
+        mockMvc.perform(get("/api/produtos/nome/{nome}", nomeProduto)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Produto não encontrado"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Cadastrar novo Produto")
+    public void cadastrarProdutoTeste() throws Exception {
+
+        when(produtoService.criar(Mockito.any(produtoDTO1.getClass()))).thenReturn(produtoDTO1);
+
+        String requestJson = objectMapper.writeValueAsString(produtoDTO1);
+        String responseJson = objectMapper.writeValueAsString(produtoDTO1);
+
+        mockMvc.perform(post("/api/produtos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+
+        verify(produtoService, times(1)).criar(Mockito.any());
+
+    }
+
+    @Test
+    @DisplayName("Atualizar um produto")
+    public void alterarProdutoTest() throws Exception {
+
+        Long idProduto = 1L;
+        when(produtoService.atualizar(Mockito.any(), Mockito.any(produtoDTO1.getClass()))).thenReturn(produtoDTO2);
+
+        String requestJson = objectMapper.writeValueAsString(produtoDTO2);
+        String responseJson = objectMapper.writeValueAsString(produtoDTO2);
+
+        mockMvc.perform(put("/api/produtos/{id}", idProduto)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
+
+        verify(produtoService, times(1)).atualizar(Mockito.any(), Mockito.any(produtoDTO2.getClass()));
+    }
+
+    @Test
+    @DisplayName("Deletar um produto existente")
+    public void deletarUmProdutoTest() throws Exception{
+
+        mockMvc.perform(delete("/api/produtos/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        Mockito.verify(produtoService, times(1)).deletar(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deletar um produto inexistente")
+    public void deletarUmProdutoInexistenteTest() throws Exception{
+
+        doThrow(new EntidadeNaoEncontradaException("Produto não encontrado"))
+                .when(produtoService).deletar(1L);
+
+        mockMvc.perform(delete("/api/produtos/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        Mockito.verify(produtoService, times(1)).deletar(Mockito.any());
     }
 
 }
