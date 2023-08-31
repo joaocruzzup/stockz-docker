610a9105bca82a7d04430402fbb25569b3166180
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-31 16:16:09 -0300

    :sparkles: feat: atualizado produtosMapper

--- src/main/java/br/com/catalisa/stockz/utils/mapper/ProdutosMapper.java
+++ src/main/java/br/com/catalisa/stockz/utils/mapper/ProdutosMapper.java
@@ -2,11 +2,16 @@ package br.com.catalisa.stockz.utils.mapper;
 
 import br.com.catalisa.stockz.model.Produto;
 import br.com.catalisa.stockz.model.dto.ProdutoDTO;
+import br.com.catalisa.stockz.model.dto.ProdutoDTOResponse;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProdutosMapper {
+
+    @Autowired
+    private CategoriaMapper categoriaMapper;
     public ProdutoDTO toProdutosDTO(Produto produto){
         ProdutoDTO categoriasDTO = new ProdutoDTO();
         BeanUtils.copyProperties(produto, categoriasDTO);
@@ -16,5 +21,14 @@ public class ProdutosMapper {
         Produto produto = new Produto();
         BeanUtils.copyProperties(produtoDTO, produto);
         return produto;
+    }
+
+    public ProdutoDTOResponse toProdutoResponse(ProdutoDTO produtoDTO){
+        ProdutoDTOResponse produtoDTOResponse = new ProdutoDTOResponse();
+        produtoDTOResponse.setNome(produtoDTO.getNome());
+        produtoDTOResponse.setDescricao(produtoDTO.getDescricao());
+        produtoDTOResponse.setPreco(produtoDTO.getPreco());
+        produtoDTOResponse.setCategoria(categoriaMapper.toCategoriasDto(produtoDTO.getCategoria()));
+        return produtoDTOResponse;
     }
 }
