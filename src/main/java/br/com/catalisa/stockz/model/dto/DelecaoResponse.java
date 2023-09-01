e28e1a32eba597e2cd2052470b19e5be3d25d2f8
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-31 21:56:42 -0300

    :sparkles: feat: adicionado exceções

---
+++ src/main/java/br/com/catalisa/stockz/model/dto/DelecaoResponse.java
@@ -0,0 +1,12 @@
+package br.com.catalisa.stockz.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DelecaoResponse {
+    private String mensagem;
+}
